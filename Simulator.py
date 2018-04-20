debcbc1d6306a2b75e92c608dbb9558736fe153b
zaeemhussain <zaeem.hussain@gmail.com>
2018-04-20 16:08:25 -0400

    Updated Simulator

--- Simulator.py
+++ Simulator.py
@@ -1,6 +1,7 @@
 import sys
 from Device import Device
 from Event import Event
+from Workflow import Workflow
 
 if __name__ == "__main__" :
 	if (len(sys.argv) != 4) :
@@ -11,22 +12,69 @@ if __name__ == "__main__" :
 		sensingTrace = sys.argv[3]
 
 	device = Device(config)
-	netTraceFile=open(networkingTrace,'r')
-	sensTraceFile=open(sensingTrace,'r')
-	netLine=netTraceFile.readline()
-	sensLine=sensTraceFile.readline()
-	netTime=0
-	sensTime=0
+	curWorkFlowId = '1'
+	curWorkFlow = device.Workflows[curWorkFlowId]
+	
+	netTraceFile = open(networkingTrace,'r')
+	sensTraceFile = open(sensingTrace,'r')
+	netLine = netTraceFile.readline()
+	sensLine = sensTraceFile.readline()
+	netTags = netLine.split(' ')
+	netTime = long(netTags[0])
+	senseTags = sensLine.split(' ')
+	sensTime = long(senseTags[0])
+	protocolPDRs = {}
+	if netTime < sensTime:
+		tcur = netTime
+		for protocolId in device.Protocols:
+			protocolPDRs[protocolId] = float(netTags[int(protocolId)])
+	else:
+		tcur=sensTime
+		for protocolId in device.Protocols:
+			protocolPDRs[protocolId] = 1.0
+	
+	granularity=100000000000000
+	sensorTicks={}
+	sensorPeriods={}
+	for sensor in device.Sensors:
+		sensorTicks[sensor] = 0
+		if device.Sensors[sensor].SensingPeriod < granularity:
+			granularity = device.Sensors[sensor].SensingPeriod
+	for sensor in device.Sensors:
+		sensorPeriods[sensor] = int(device.Sensors[sensor].SensingPeriod/granularity)
+	
+	tcur=min(netTime,sensTime)
+	
 	eventList=[]
+	missed = 0
 	while netLine or sensLine:
-		if netLine:
-			netTags=netLine.split(' ')
-			netTime=long(netTags[0])
-		if sensLine:
-			senseTags=sensLine.split(' ')
-			sensTime=long(senseTags[0])
-		if sensTime < netTime:
-			x=Event('Sensing)
+		for sensor in device.Sensors:
+			sensorTicks[sensor] = sensorTicks[sensor] + 1
+			if sensorTicks[sensor] == sensorPeriods[sensor]:
+				sensorTicks[sensor] = 0
+				if curWorkFlow.SensorId == sensor:
+					#calculate workflow energy
+					if sensLine and tcur > int(senseTags[1]):
+						sensLine = sensTraceFile.readline()
+						if sensLine:
+							senseTags = sensLine.split(' ')
+					if sensLine:
+						if tcur > int(senseTags[1]):
+							missed = missed + 1
+						else if tcur > int(senseTags[0]):
+							#use rules with incident motion to change workflow
+						else:
+							#use rules with incident still to change workflow
+		tcur = tcur + granularity
+		while tcur > netTime and netLine:
+			for protocolId in device.Protocols:
+				protocolPDRs[protocolId] = float(netTags[int(protocolId)])
+			netLine = netTraceFile.readline()
+			if netLine:
+				netTags = netLine.split(' ')		
+				netTime = long(netTags[0])
+				
+				
 	print(str(device))
 
 
